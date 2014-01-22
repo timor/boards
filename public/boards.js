b2495c6b8b06cf784431b4c8cc6504abe836b037
timor <timor.dd@googlemail.com>
2014-01-22 01:08:42 +0100

    buttons, adding and removing

--- public/boards.js
+++ public/boards.js
@@ -1,12 +1,29 @@
 function update() {
-    $('#sidebar').load("views/sidebar");
+    $('.dynamic').each(function(){
+        url="view/"+this.id.replace("_","/");
+        $(this).load(url)});
 }
+function displayBoard(id) {
+    $('#main').load("view/board/"+id)
+}
+    
 
 $(function() {
-    $("input[type=submit], button" ).button();
-    $('#sidebar').on("click","button#create_board",function(event) {
-        $.post('boards/create_empty');
-        update()
-    });
+    $("input[type=submit], button" ).livequery(function(){$(this).button()});
+    // $('button#create_board').on("click",function() {
+    //     $.post('boards/create_empty');
+    //     update()});
+    $('button#reload').on("click",update);
+    $('#boards_list')
+        .on("click",".board-selector",function(e) {displayBoard(e.target.dataset.boardId)})
+        // .on("click","#create_board",function() {$.post('boards/create_empty'); update()})
+        .on("click","button.trash",function(e) {$.post('delete/board/'+e.target.dataset.boardId);
+                                                update()})
+    ;
+    $('button.trash').livequery(function(){
+        $(this).button({icons: { primary: "ui-icon-trash"}, text: false})});
+    $('button#create_board').livequery(function(){
+        $(this).button({icons: { primary: "ui-icon-plusthick"}, text: false})
+            .click(function(){$.post('boards/create_empty'); update()})});
     update();
 });
