c88027126b68dfe18b0fae3decdf255cf483a2a8
timor <timor.dd@googlemail.com>
2014-01-22 19:17:41 +0100

    adding cards works, better update code

--- boards.rb
+++ boards.rb
@@ -10,6 +10,9 @@ enable :sessions
 set :session_secret, 'board secret'
 set :slim, :pretty => true
 
+# potentially very slow
+STDOUT.sync=true
+
 DataMapper::Logger.new($stdout, :debug)
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/boards.db")
 
@@ -30,7 +33,15 @@ class Board
   property :id, Serial
   property :title, String
   property :created_at, DateTime
-  has n, :columns
+  has n, :columns, constraint: :destroy
+
+  def self.create_default(args)
+    board=create(args);
+    board.columns.create(:title => 'Todo');
+    board.columns.create(:title => 'Doing');
+    board.columns.create(:title => 'Done');
+    board
+  end
 end
 
 class Column
@@ -45,28 +56,63 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
+require 'pp'
+get '/view/main' do
+  bid=session[:current_board]
+  if bid
+    redirect "/view/board/#{bid}"
+  else
+    slim "h1 Please select or create a board!"
+  end
+end
+
 get '/view/*/*' do |resource,vtype|
+  puts "view request"
   case resource.to_sym
   when :boards then 
     @boards=Board.all
   when :cards then
     @cards=Card.all
+  when :board then
+    @board=Board.get(vtype)
+    return "board not found" unless @board
+    puts "board #{@board.title} with #{@board.columns.length} columns"
+    session[:current_board]=vtype
+    vtype="view"
   else return 404 end
   slim (resource+"_"+vtype).to_sym
 end
 
 post '/boards/create_empty' do
   puts "creating empty board"
-  Board.create(:title => "New Board")
+  board=Board.create_default(:title => "New Board")
 end
 
 post '/delete/board/*' do |id|
-  board=Board.get(id)
-  board.destroy if board
+  puts "deleting board"
+  done=Board.get(id).destroy
+  if !done
+    puts "board not deleted"
+    return [500,["board not deleted"]]
+  end
 end
 
-post '/boards/*/create_column' do
+post '/boards/*/create_column' do |board_id|
+  puts "creating column in board #{board_id}...NOT!"
 end
+
+post '/columns/*/create_card' do |col_id|
+  puts "creating card in column #{col_id}"
+  Column.get(col_id).cards.create(title: "card title", body: "card body")
+end
+
+# explicitely disable caching for ajax requests: fuck you IE!
+after '/view/*' do
+  if request.env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    response["Expires"]="-1"
+  end
+end
+
   
 get '/' do
   slim :index
--- public/boards.js
+++ public/boards.js
@@ -3,27 +3,30 @@ function update() {
         url="view/"+this.id.replace("_","/");
         $(this).load(url)});
 }
+
 function displayBoard(id) {
     $('#main').load("view/board/"+id)
 }
-    
 
 $(function() {
+    $(document).ajaxError(function(evt,xhr) {
+        $("div.log").append($('<p>',{class: 'error'}).text(xhr.responseText))});
     $("input[type=submit], button" ).livequery(function(){$(this).button()});
     // $('button#create_board').on("click",function() {
     //     $.post('boards/create_empty');
     //     update()});
     $('button#reload').on("click",update);
     $('#boards_list')
-        .on("click",".board-selector",function(e) {displayBoard(e.target.dataset.boardId)})
-        // .on("click","#create_board",function() {$.post('boards/create_empty'); update()})
-        .on("click","button.trash",function(e) {$.post('delete/board/'+e.target.dataset.boardId);
-                                                update()})
-    ;
+    // thanks IE for not supporting dataset!
+        .on("click",".board-selector",function(e) {displayBoard(e.target.getAttribute('data-board-id'));return false;})
+    // .on("click","#create_board",function() {$.post('boards/create_empty'); update()})
+        .on("click","button.trash",function(e) {$.post('delete/board/'+e.target.getAttribute('data-board-id'),{},update)});
+    $('#main')
+        .on("click",".add-card",function(e) {$.post('columns/'+e.target.getAttribute('data-column-id')+'/create_card',{},update)});
     $('button.trash').livequery(function(){
         $(this).button({icons: { primary: "ui-icon-trash"}, text: false})});
     $('button#create_board').livequery(function(){
         $(this).button({icons: { primary: "ui-icon-plusthick"}, text: false})
-            .click(function(){$.post('boards/create_empty'); update()})});
+            .click(function(){$.post('boards/create_empty',{},update)})});
     update();
 });
